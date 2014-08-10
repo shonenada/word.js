c2961e855fef59e386a5eec701a8b16eda702c8f
shonenada <shonenada@gmail.com>
2014-08-10 12:36:44 +0800

    finished demo

---
+++ word/static/scripts/app.js
@@ -0,0 +1,21 @@
+// Create module for wordApp
+var wordApp = angular.module('wordApp', [
+    'ngRoute', 'wordController'
+]);
+
+wordApp.config(
+    ['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/', {
+            controller: 'WordShowController',
+            templateUrl: 'templates/word.html'
+        }).
+        when('/finished', {
+            controller: 'FinishedController',
+            templateUrl: 'templates/finished.html'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    }]
+);
